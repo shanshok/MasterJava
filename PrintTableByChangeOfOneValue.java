45b5a86c23d995373ea7940eb48b0298a6f0e081
shanshok <39254070+shanshok@users.noreply.github.com>
2018-05-23 11:39:23 +0530

    Add files via upload

---
+++ PrintTableByChangeOfOneValue.java
@@ -0,0 +1,14 @@
+package javaashok;
+
+public class PrintTableByChangeOfOneValue {
+
+	public static void main(String[] args) {
+		
+		/*No. of table stored in variable a, need to update values to get values */
+		int a = 10;
+		for (int i=1; i<=10; i++)		
+		System.out.println (a*i);		
+
+	}
+
+}
