924fa88d100f1667ce64285b5ef83d152422ba8b
shanshok <39254070+shanshok@users.noreply.github.com>
2018-05-23 11:51:30 +0530

    Add files via upload

---
+++ Print2N2.java
@@ -0,0 +1,15 @@
+package javaashok;
+
+public class Print2N2 {
+
+	public static void main(String[] args) {
+    
+	int a=2;
+    int b=2;
+    
+    for (int i=1; i<=5; i++)
+    System.out.println(a*(i*b));  
+ 
+	}
+
+}
